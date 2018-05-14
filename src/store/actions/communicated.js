62bdf2ad004ce64a786ce5a4333e14b76c7ca95f
AWen <961661773@qq.com>
2018-05-14 17:17:54 +0800

    '创建两个报名页面'

--- src/store/actions/communicated.js
+++ src/store/actions/communicated.js
@@ -4,23 +4,36 @@ export const communicatedData = createAction(COMMUNICATED_DATA, (res) => {
   const comData = [
     {
       id: 0,
-      comName: '嗯哈xxxxxx',
-      comMsg: '领域:TMT'
+      mid: 0,
+      name: '',
+      company: '嗯哈xxxxxx嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT', 'TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
+      // 面谈记录id，面谈人id，面谈人详情
     },
     {
       id: 1,
-      comName: '嗯哈xxxxxx',
-      comMsg: '领域:TMT'
+      mid: 1,
+      name: '',
+      company: '嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
     },
     {
       id: 2,
-      comName: '嗯哈xxxxxx',
-      comMsg: '领域:TMT'
+      mid: 2,
+      name: '',
+      company: '嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
     },
     {
       id: 3,
-      comName: '嗯哈xxxxxx',
-      comMsg: '领域:TMT'
+      mid: 3,
+      name: '',
+      company: '嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
     }
   ]
   return new Promise(resolve => {
--- src/store/reducers/communicated.js
+++ src/store/reducers/communicated.js
@@ -8,5 +8,39 @@ export default handleActions({
     }
   }
 }, {
-  comData: []
+  comData: [
+    {
+      id: 0,
+      mid: 0,
+      name: '',
+      company: '嗯哈xxxxxx嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT', 'TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
+      // 面谈记录id，面谈人id，面谈人详情
+    },
+    {
+      id: 1,
+      mid: 1,
+      name: '',
+      company: '嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
+    },
+    {
+      id: 2,
+      mid: 2,
+      name: '',
+      company: '嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
+    },
+    {
+      id: 3,
+      mid: 3,
+      name: '',
+      company: '嗯哈xxxxxx',
+      focusArea: ['TMT', 'TMT', 'TMT'],
+      rounds: ['种子轮', 'D轮', 'D轮']
+    }
+  ]
 })
