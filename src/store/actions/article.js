4815f37798ae20ff2a9830bf7c7076cd2a2be890
AWen <961661773@qq.com>
2018-05-17 17:42:25 +0800

    '新增文章页面组件'

--- src/store/actions/article.js
+++ src/store/actions/article.js
@@ -3,9 +3,10 @@ import { createAction } from 'redux-actions'
 export const article = createAction(ARTICLE, (res) => {
   const articleData = {
     src: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-    text: '动点科技文章动点科技文章动点科技文章动点科技文章动点科技文章',
+    title: '动点科技文章动点科技文章动点科技文章动点科技文章动点科技文章',
     describe: '好的好的好的好的',
-    time: ''
+    time: '',
+    content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'
   }
   return new Promise(resolve => resolve(articleData))
 })
