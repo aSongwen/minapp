12d3063222b249d306cb1fb434a7f73a8326b6c2
riguang <102936778@qq.com>
2018-05-29 12:29:34 +0800

    创业者报名信息获取

--- src/api/startup.js
+++ src/api/startup.js
@@ -4,8 +4,8 @@
 import base from './base'
 import url from './url'
 export default class startup extends base {
-  static getInfo(id) {
-    const _url = `${url.startup}/${id}`
+  static getInfo() {
+    const _url = `${url.startup}`
     return this.get(_url).then(data => data)
   }
   static apply(param) {
