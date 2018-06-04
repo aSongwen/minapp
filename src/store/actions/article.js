349725d01fd97d36fb313c85a9544e2cbbb5feb1
AWen <961661773@qq.com>
2018-06-04 15:31:40 +0800

    update

--- src/store/actions/article.js
+++ src/store/actions/article.js
@@ -2,9 +2,8 @@ import { ARTICLE } from '../types/index'
 import { createAction } from 'redux-actions'
 export const article = createAction(ARTICLE, (res) => {
   const articleData = {
-    src: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-    title: '动点科技文章动点科技文章动点科技文章动点科技文章动点科技文章',
-    describe: '好的好的好的好的',
+    src: 'https://cn.technode.com/wp-content/blogs.dir/18/files/2018/05/pic2-5-1024x693.jpg',
+    title: '透过小米 8 探索版半透明后盖看到的并不是真实的手机内部',
     time: '',
     content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'
   }
--- src/store/actions/home-page.js
+++ src/store/actions/home-page.js
@@ -1,14 +1,29 @@
 import { ASYNC_HOME_SLIDERIMG, ASYNC_HOME_VCIMG } from '../types/index'
 import { createAction } from 'redux-actions'
 export const asyncHomeSliderImg = createAction(ASYNC_HOME_SLIDERIMG, (res) => {
+  // const silderImg = [
+  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/6401.jpg',
+  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/cyanhill1.jpg',
+  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2018/03/投资人1.jpg',
+  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2016/07/rongzi.jpg'
+  // ]
   const silderImg = [
-    'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/6401.jpg',
-    'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/cyanhill1.jpg',
-    'https://static.technode.com/wp-content/blogs.dir/18/files/2018/03/投资人1.jpg',
-    'https://static.technode.com/wp-content/blogs.dir/18/files/2016/07/rongzi.jpg',
-    'https://static.technode.com/wp-content/blogs.dir/18/files/2018/03/投资人.jpg"',
-    'https://static.technode.com/wp-content/blogs.dir/18/files/2018/03/zhenfund-edu-1.jpg',
-    'https://static.technode.com/files/2017/12/sourcecodecapital-cover.jpeg'
+    {
+      img: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/6401.jpg',
+      content: '文字文字文字文字文字文字文字'
+    },
+    {
+      img: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/cyanhill1.jpg',
+      content: '文字文字文字文字文字文字文字'
+    },
+    {
+      img: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/03/投资人1.jpg',
+      content: '文字文字文字文字文字文字文字'
+    },
+    {
+      img: 'https://static.technode.com/wp-content/blogs.dir/18/files/2016/07/rongzi.jpg',
+      content: '文字文字文字文字文字文字文字'
+    }
   ]
   return new Promise(resolve => {
     setTimeout(() => {
@@ -20,21 +35,24 @@ export const asyncHomeSliderImg = createAction(ASYNC_HOME_SLIDERIMG, (res) => {
 export const asyncHomeVcImg = createAction(ASYNC_HOME_VCIMG, (res) => {
   const vcImg = [
     {
+      id: 0,
+      src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/didi_nanjing-326x235.jpg',
+      text: '滴滴外卖登陆南京',
+      describe: '南京多部门要求禁止“价格战”。',
+      time: ''
+    },
+    {
+      id: 1,
+      src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/屏幕快照-2018-06-01-上午9.44.39-326x235.png',
+      text: 'AI + 零售如何共舞？知己知彼，方能走得长久',
+      describe: '对于零售行业来说，新技术是块砖，哪里需要哪里搬。那么， AI 如何助力零食产业升级，撬动这个巨大的市场呢？',
+      time: ''
+    },
+    {
+      id: 2,
       src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/pixel2-326x235.jpg',
-      text: '投资人专栏投资人专栏投资人',
-      describe: '好的好的好的好的',
-      time: ''
-    },
-    {
-      src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/cc99f851c2c74d3-326x235.gif',
-      text: '投资人专栏投资人专栏投资人',
-      describe: '好的好的好的好的',
-      time: ''
-    },
-    {
-      src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2017/02/bigdata-326x235.jpg',
-      text: '投资人专栏投资人专栏投资人',
-      describe: '好的好的好的好的',
+      text: '彭博：Google Pixel 3 XL 也将采用刘海屏，预计 10 月发布',
+      describe: 'Pixel 3 将继续采用单摄像头。',
       time: ''
     }
   ]
