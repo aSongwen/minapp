2cad721aeccf670ca3f791a7c2561a52016702c9
AWen <961661773@qq.com>
2018-06-05 15:11:24 +0800

    [MOD]投资人专栏组件

--- src/store/actions/home-page.js
+++ src/store/actions/home-page.js
@@ -1,12 +1,6 @@
 import { ASYNC_HOME_SLIDERIMG, ASYNC_HOME_VCIMG } from '../types/index'
 import { createAction } from 'redux-actions'
 export const asyncHomeSliderImg = createAction(ASYNC_HOME_SLIDERIMG, (res) => {
-  // const silderImg = [
-  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/6401.jpg',
-  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/cyanhill1.jpg',
-  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2018/03/投资人1.jpg',
-  //   'https://static.technode.com/wp-content/blogs.dir/18/files/2016/07/rongzi.jpg'
-  // ]
   const silderImg = [
     {
       img: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/05/6401.jpg',
@@ -35,25 +29,23 @@ export const asyncHomeSliderImg = createAction(ASYNC_HOME_SLIDERIMG, (res) => {
 export const asyncHomeVcImg = createAction(ASYNC_HOME_VCIMG, (res) => {
   const vcImg = [
     {
-      id: 0,
-      src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/didi_nanjing-326x235.jpg',
-      text: '滴滴外卖登陆南京',
-      describe: '南京多部门要求禁止“价格战”。',
-      time: ''
+      title: '滴滴外卖登陆南京',
+      link: '',
+      pubDate: 'Tue, 22 May 2018 04:10:27 +0000',
+      cover: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/didi_nanjing-326x235.jpg'
+
     },
     {
-      id: 1,
-      src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/屏幕快照-2018-06-01-上午9.44.39-326x235.png',
-      text: 'AI + 零售如何共舞？知己知彼，方能走得长久',
-      describe: '对于零售行业来说，新技术是块砖，哪里需要哪里搬。那么， AI 如何助力零食产业升级，撬动这个巨大的市场呢？',
-      time: ''
+      title: 'AI + 零售如何共舞？知己知彼，方能走得长久',
+      link: '',
+      pubDate: '',
+      cover: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/屏幕快照-2018-06-01-上午9.44.39-326x235.png'
     },
     {
-      id: 2,
-      src: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/pixel2-326x235.jpg',
-      text: '彭博：Google Pixel 3 XL 也将采用刘海屏，预计 10 月发布',
-      describe: 'Pixel 3 将继续采用单摄像头。',
-      time: ''
+      title: '彭博：Google Pixel 3 XL 也将采用刘海屏， 预计 10 月发布',
+      link: '',
+      pubDate: '',
+      cover: 'https://static.technode.com/wp-content/blogs.dir/18/files/2018/06/pixel2-326x235.jpg'
     }
   ]
   return new Promise(resolve => {
