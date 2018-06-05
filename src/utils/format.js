09a7e6dd4d28c9d1ab30f26718e05ea77d0cbee0
AWen <961661773@qq.com>
2018-06-05 15:34:24 +0800

    update

--- src/utils/format.js
+++ src/utils/format.js
@@ -22,7 +22,7 @@ function formatMsgTime (timespan) {
     timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60)) + '小时前'
   } else if (1000 * 60 * 60 * 24 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24 * 15) {
     timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60 * 24)) + '天前'
-  } else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year == now.getFullYear()) {
+  } else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year === now.getFullYear()) {
     timeSpanStr = month + '-' + day + ' ' + hour + ':' + minute
   } else {
     timeSpanStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute
